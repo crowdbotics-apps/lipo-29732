ec4b1b360dfda6c0276f3512a016e2cac0789cc1
Crowdbotics <team@crowdbotics.com>
2021-08-11 21:54:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lipo_29732Identifier",
+  url: "https://lipo_29732Identifier.botics.co",
+  api: "https://lipo_29732Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
